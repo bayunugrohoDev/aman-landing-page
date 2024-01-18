ac090b803415ab5f228202aa05ccbca179a0776a
bayunugrohoDev <bayunugroho963@gmail.com>
2024-01-18 17:57:31 +0700

    add swiper for testimonial component

---
+++ src/components/TestimonialItem.jsx
@@ -0,0 +1,30 @@
+import Image from "next/image";
+
+const TestimonialItem = ({imgSrc, message, name}) => {
+  return (
+    <div className="w-full flex gap-6 px-16 ">
+      <div className=" min-w-[114px] relative">
+        <Image
+          width="114"
+          height="96"
+          src={`/images/testimoni-bg-user.png`}
+          alt="bg blue testimoni user"
+          className=""
+        ></Image>
+        <Image
+          width="96"
+          height="96"
+          src={imgSrc}
+          alt="testimoni 1"
+          className="absolute top-2 left-2 rounded-full"
+        ></Image>
+      </div>
+      <div className="flex flex-col gap-6 w-full max-w-[582px]">
+        <p className="text-[22px]">{message}</p>
+        <p className="text-md">{name}</p>
+      </div>
+    </div>
+  );
+};
+
+export default TestimonialItem;
--- src/components/Testimonials.jsx
+++ src/components/Testimonials.jsx
@@ -2,9 +2,14 @@
 
 import { useTranslations } from "next-intl";
 import Image from "next/image";
+import "swiper/css";
+import { Autoplay, Navigation, Pagination } from "swiper/modules";
 import React, { useState } from "react";
-import Slider from "react-slick";
+
 import LeftArrowIcon from "./icons/LeftArrowIcon";
+import { Swiper, SwiperSlide, useSwiper } from "swiper/react";
+import TesttimonialItem from "./TestimonialItem";
+
 const reviews = [
   {
     img: "/images/testimoni-user-1.png",
@@ -19,78 +24,61 @@ const reviews = [
 ];
 
 const Testimonials = () => {
+  // const swiper = useSwiper();
   const t = useTranslations("testimonials");
 
-  const [index, setIndex] = useState(0);
+  const [swiper, setSwiper] = useState(null);
 
-  function leftShiftHandler() {
-    if (index - 1 < 0) setIndex(reviews.length - 1);
-    else setIndex(index - 1);
-  }
-  function rightShiftHandler() {
-    if (index + 1 >= reviews.length) setIndex(0);
-    else setIndex(index + 1);
-  }
+  const leftShiftHandler = () => {
+    if (swiper) {
+      swiper.slidePrev();
+    }
+  };
 
+  const rightShiftHandler = () => {
+    if (swiper) {
+      swiper.slideNext();
+    }
+  };
   return (
-    <div className="transition-all duration-700 relative">
-      <div className={`w-full flex flex-col `}>
-        <div className="w-full flex gap-6 px-16 ">
-          <div className=" min-w-[114px] relative">
-            <Image
-              width="114"
-              height="96"
-              src={`/images/testimoni-bg-user.png`}
-              alt="bg blue testimoni user"
-              className=""
-            ></Image>
-            <Image
-              width="96"
-              height="96"
-              src={"/images/testimoni-user-1.png"}
-              alt="testimoni 1"
-              className="absolute top-2 left-2"
-            ></Image>
-          </div>
-          <div className="flex flex-col gap-6 w-full max-w-[582px]">
-            <p className="text-[22px]">{t("reviews.0.message")}</p>
-            <p className="text-md">{t("reviews.0.name")}</p>
-          </div>
-        </div>
-        {/* <div className="w-full flex gap-6 px-16 ">
-          <div className=" min-w-[114px] relative">
-            <Image
-              width="114"
-              height="96"
-              src={`/images/testimoni-bg-user.png`}
-              alt="bg blue testimoni user"
-              className=""
-            ></Image>
-            <Image
-              width="96"
-              height="96"
-              src={"/images/testimoni-user-1.png"}
-              alt="testimoni 1"
-              className="absolute top-2 left-2"
-            ></Image>
-          </div>
-          <div className="flex flex-col gap-6 w-full max-w-[582px]">
-            <p className="text-[22px]">{t("reviews.1.message")}</p>
-            <p className="text-md">{t("reviews.1.name")}</p>
-          </div>
-        </div> */}
-      </div>
-      {/* button  */}
-      <div className="flex items-center gap-6 absolute w-[152px] top-0 ltr:right-6 rtl:left-6">
+    <div className="relative">
+      <Swiper
+        spaceBetween={50}
+        slidesPerView={1}
+        autoplay={{
+          delay: 2500,
+          disableOnInteraction: false,
+        }}
+        loop={true}
+        onSlideChange={() => console.log("slide change")}
+        onSwiper={(swiper) => setSwiper(swiper)}
+        modules={[Autoplay, Navigation, Pagination]}
+      >
+        <SwiperSlide>
+          <TesttimonialItem
+            imgSrc={"/images/testimoni-user-1.png"}
+            message={t("reviews.0.message")}
+            name={t("reviews.0.name")}
+          />
+        </SwiperSlide>
+        <SwiperSlide>
+          <TesttimonialItem
+            imgSrc={"/images/testimoni-user-2.png"}
+            message={t("reviews.1.message")}
+            name={t("reviews.1.name")}
+          />
+        </SwiperSlide>
+      </Swiper>
+      <div className="flex z-20 items-center gap-6 absolute w-[152px] top-0 ltr:right-6 rtl:left-6">
         <button
-          onClick={rightShiftHandler}
+          onClick={leftShiftHandler}
           className="bg-lightGrey w-16 h-16 border-2 border-grey rounded-full flex items-center justify-center"
         >
           <LeftArrowIcon />
         </button>
 
         <button
-          onClick={leftShiftHandler}
+          onClick={rightShiftHandler}
           className="bg-lightGrey w-16 h-16 border-2 border-grey rounded-full flex items-center justify-center rotate-180"
         >
           <LeftArrowIcon />
