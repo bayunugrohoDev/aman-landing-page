1c903e764c9c34b7fc42270401d5f3d3d85ceefb
bayunugrohoDev <bayunugroho963@gmail.com>
2024-02-06 10:24:14 +0700

    change navbar logo from text to svg

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -9,6 +9,21 @@ import { Link, usePathname } from "../../navigation";
 import SupportIcon from "./icons/SupportIcon";
 import BurgerIcon from "./icons/BurgerIcon";
 
+const logo_resolutions = {
+  en: {
+    width: 138.95,
+    height: 35,
+  },
+  ar: {
+    width: 106.05,
+    height: 42.52,
+  },
+  ku: {
+    width: 123.72,
+    height: 37.45,
+  },
+};
+
 const Navbar = ({ className = "" }) => {
   const currentLocal = useLocale();
   const pathname = usePathname();
@@ -25,14 +40,14 @@ const Navbar = ({ className = "" }) => {
       <div className="container mx-auto flex flex-wrap items-center justify-between p-4 ">
         <Link href="/" className="flex items-center gap-1">
           <Image
-            height={35}
-            width={35}
-            src="/images/logo.svg"
+            height={logo_resolutions[currentLocal].height}
+            width={logo_resolutions[currentLocal].width}
+            src={`/images/logo_${currentLocal}.svg`}
             alt="Aman Logo"
           />
-          <span className="self-center whitespace-nowrap pt-2 text-3xl font-bold">
+          {/* <span className="self-center whitespace-nowrap pt-2 text-3xl font-bold">
             {t("title")}
-          </span>
+          </span> */}
         </Link>
         <button
           className="inline-flex h-10 w-10 items-center justify-center rounded-lg p-2 text-sm text-gray-500 hover:bg-gray-100 focus:outline-none focus:ring-2 focus:ring-gray-200 md:hidden"
